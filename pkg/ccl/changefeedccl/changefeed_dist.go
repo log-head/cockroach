14069926c35a4b8eec17df4eade15d68bdb66b6d
Matthew Lougheed <matthew.lougheed@cockroachlabs.com>
2025-08-18 12:36:40 -0400

    changefeedccl: block db-level changefeed creation for non-empty tableset
    On startup, the changefeed should not be started until there exist
    tables in the database to watch. If no tables exist at the time of
    startup, then a tableset watcher is created and polled; the changefeed
    will be started from where there is first a target table.
    
    Fixes: #147371
    Epic: CRDB-1421
    
    Release note: none

--- pkg/ccl/changefeedccl/changefeed_dist.go
+++ pkg/ccl/changefeedccl/changefeed_dist.go
@@ -84,6 +84,7 @@ func distChangefeedFlow(
 	resultsCh chan<- tree.Datums,
 	onTracingEvent func(ctx context.Context, meta *execinfrapb.TracingAggregatorEvents),
 	targets changefeedbase.Targets,
+	schemaTSOverride hlc.Timestamp,
 ) error {
 	opts := changefeedbase.MakeStatementOptions(details.Opts)
 	progress := localState.progress
@@ -136,6 +137,11 @@ func distChangefeedFlow(
 			knobs.StartDistChangefeedInitialHighwater(ctx, initialHighWater)
 		}
 	}
+	if !schemaTSOverride.IsEmpty() {
+		fmt.Printf("overriding schemaTS from %s to %s\n", schemaTS, schemaTSOverride)
+		schemaTS = schemaTSOverride
+		initialHighWater = schemaTSOverride
+	}
 	return startDistChangefeed(
 		ctx, execCtx, jobID, schemaTS, details, description, initialHighWater, localState, resultsCh, onTracingEvent, targets)
 }
@@ -159,10 +165,12 @@ func fetchTableDescriptors(
 		// and lie within the primary index span. Deduplication is important
 		// here as requesting the same span twice will deadlock.
 		return targets.EachTableID(func(id catid.DescID) error {
+			fmt.Printf("fetching table descriptor %d at timestamp %s\n", id, ts)
 			tableDesc, err := descriptors.ByIDWithoutLeased(txn.KV()).WithoutNonPublic().Get().Table(ctx, id)
 			if err != nil {
 				return errors.Wrapf(err, "fetching table descriptor %d", id)
 			}
+			fmt.Printf("fetched table descriptor %d at timestamp %s\n", id, ts)
 			targetDescs = append(targetDescs, tableDesc)
 			return nil
 		})
@@ -269,11 +277,13 @@ func startDistChangefeed(
 			log.Changefeed.Infof(ctx, "span-level checkpoint: %s", spanLevelCheckpoint)
 		}
 	}
+	fmt.Printf("making plan\n")
 	p, planCtx, err := makePlan(execCtx, jobID, details, description, initialHighWater,
 		trackedSpans, spanLevelCheckpoint, localState.drainingNodes, schemaTS)(ctx, dsp)
 	if err != nil {
 		return err
 	}
+	fmt.Printf("made plan\n")
 
 	execPlan := func(ctx context.Context) error {
 		// Derive a separate context so that we can shut down the changefeed
@@ -339,6 +349,7 @@ func startDistChangefeed(
 		return resultRows.Err()
 	}
 
+	fmt.Printf("end of startDistChangefeed\n")
 	return ctxgroup.GoAndWait(ctx, execPlan)
 }
 
